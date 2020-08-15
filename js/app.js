e2b28256f519a15698517baf8632c12de470b472
Nerio Balza <neriojbd@gmail.com>
2020-08-15 18:58:41 -0400

    Pagina Rediseñada

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 var menu = document.getElementById("menu");
 var burgerButton = document.getElementById("burger");
+var linkslocales = document.getElementsByClassName("link");
 
 burgerButton.addEventListener("click",function(){
     menu.classList.toggle("is-active");
+});
+for(var i = 0; i<linkslocales.length; i++)
+linkslocales[i].addEventListener("click",function(){
+    if(window.innerWidth<768){
+        menu.classList.toggle("is-active");
+    }
 });
